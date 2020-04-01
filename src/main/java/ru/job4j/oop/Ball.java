0facb14692a16f74ba8bd6c5b36aa1064f2bd16d
anastasia-levina <redheadgirl.lv@gmail.com>
2020-04-01 19:15:34 +0300

    1.6. Взаимодействие объектов.[#252336]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void runAway() {
+
+    }
+
+    public void runAway(Hare hare) {
+
+    }
+
+    public void runAway(Wolf wolf) {
+    }
+
+    public void restInPeace() {
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,19 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+
+        ball.runAway();
+        hare.tryEat(ball);
+        ball.runAway(hare);
+        wolf.tryEat(ball);
+        ball.runAway(wolf);
+        fox.tryEat(ball);
+        ball.restInPeace();
+    }
+}

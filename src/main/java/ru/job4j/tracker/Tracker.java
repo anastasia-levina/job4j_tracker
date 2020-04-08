d212ee5f146c384064a9b9ccaa6a8508be4f8a86
anastasia-levina <a.belov.nsk@yandex.ru>
2020-04-08 12:36:21 +0300

    5. Реализовать класс Tracker[#252356]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -15,24 +15,15 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] itemsWithoutNull = new Item[items.length];
-        int count = 0;
-        for (int index = 0; index < items.length; index++) {
-            Item item = items[index];
-            if (item != null) {
-                itemsWithoutNull[count] = item;
-                count++;
-            }
-        }
-        return Arrays.copyOf(itemsWithoutNull, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
-        Item[] itemsArray = new Item[items.length];
+        Item[] itemsArray = new Item[size];
         int count = 0;
-        for (int index = 0; index < items.length; index++) {
+        for (int index = 0; index < size; index++) {
             Item item = items[index];
-            if (item != null && item.getName().equals(key)) {
+            if (item.getName().equals(key)) {
                 itemsArray[count] = item;
                 count++;
             }

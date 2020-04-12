a3dea5047a42c7707ccad92ef34045fb4d96be46
anastasia-levina <a.belov.nsk@yandex.ru>
2020-04-12 20:08:14 +0300

    2. Scanner и чтение числа из консоли.[#252293]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Matches {
@@ -8,43 +9,43 @@ public class Matches {
         int match = 11;
         System.out.println("Игра ''Спички''");
         System.out.println("На столе лежат спички. Всего их " + match + ". Из них возьмите от 1 до 3 штук.");
+        int counter = 0;
         while (true) {
-            System.out.print("Введите количество спичек для первого игрока: ");
-            int number1 = input.nextInt();
-            if (number1 > 3 || number1 <= 0) {
-                System.out.print("Введено неверное количество спичек, введите количество от 1 до 3: ");
-                number1 = input.nextInt();
-            }
-            int residual = match - number1;
-            if (residual >= 0) {
-                System.out.println("Осталось " + (residual) + " спичек.");
-            }
-            if (residual < 0) {
-                System.out.println("Вы превысили лимит оставшихся спичек! Не делайте так больше. Игра завершена, никто не победил. ");
-                break;
-            } else if (number1 == match) {
-                System.out.println("Игра завершена. Победил первый игрок!");
+            int gamer = counter % 2 + 1;
+            System.out.print("Введите количество спичек для " + gamer + "-го игрока: ");
+            int number1 = userInput(input);
+            if (number1 == match) {
+                System.out.println("Игра завершена. Победил " + gamer + "-й игрок!");
                 break;
             }
-            System.out.print("Введите количество спичек для второго игрока: ");
-            int number2 = input.nextInt();
-            if (number2 > 3 || number2 <= 0) {
-                System.out.print("Введено неверное количество спичек, введите количество от 1 до 3: ");
-                number2 = input.nextInt();
-            }
-            int residual2 = match - number1 - number2;
-            if (residual2 >= 0) {
-                System.out.println("Осталось " + (residual2) + " спичек.");
-            }
-            if (residual2 < 0) {
-                System.out.println("Вы превысили лимит оставшихся спичек! Не делайте так больше. Игра завершена, никто не победил. ");
-                break;
-            } else if (number2 == residual) {
-                System.out.println("Игра завершена. Победил второй игрок!");
+            match -= number1;
+            if (!overLimit(match)) {
                 break;
             }
-            match = match - (number1 + number2);
+            counter++;
         }
-        System.out.println();
+    }
+
+    public static int userInput(Scanner input) {
+        while (true) {
+            try {
+                int i = Integer.parseInt(input.nextLine());
+                if (i >= 1 && i <= 3) return i;
+                else throw new IOException();
+            } catch (IOException | NumberFormatException ex) {
+                System.out.println("Неправильный ввод. Введите, пожалуйста, число от 1 до 3: ");
+            }
+        }
+    }
+
+    public static boolean overLimit(int residual) {
+        if (residual > 0) {
+            System.out.println("Осталось " + (residual) + " спичек.");
+        }
+        if (residual < 0) {
+            System.out.println("Вы превысили лимит оставшихся спичек! Не делайте так больше. Игра завершена, никто не победил. ");
+            return false;
+        }
+        return true;
     }
 }

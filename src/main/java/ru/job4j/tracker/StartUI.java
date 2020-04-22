0be8292cd14c8f1bcd35ea85b32548989afbe00a
anastasia-levina <a.belov.nsk@yandex.ru>
2020-04-22 22:22:53 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.[#252317]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = Integer.parseInt(input.askStr("Select: "));
+            int select = input.askInt("Select: ", actions.length);
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -27,7 +27,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input validate = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(), new ShowAction(),
@@ -35,6 +35,6 @@ public class StartUI {
                 new FindIdAction(), new FindNameAction(),
                 new ExitAction()
         };
-        new StartUI().init(input, tracker, actions);
+        new StartUI().init(validate, tracker, actions);
     }
 }

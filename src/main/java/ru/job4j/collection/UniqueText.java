903b03086c5a5129b85972cd594b995f346cafec
anastasia-levina <a.belov.nsk@yandex.ru>
2020-06-11 17:40:58 +0300

    2. Идентичные тексты.[212655#252276]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,20 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String oText : origin) {
+            check.add(oText);
+        }
+        for (String dText : text) {
+            rsl = check.contains(dText);
+            if (!rsl) break;
+        }
+        return rsl;
+    }
+}

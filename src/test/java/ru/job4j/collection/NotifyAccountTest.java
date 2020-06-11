d867f05fe0aa5408c3a6826ab250736b4a839216
anastasia-levina <a.belov.nsk@yandex.ru>
2020-06-11 19:58:16 +0300

    3. Модели данных и HashSet.[218530#252277]

---
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class NotifyAccountTest {
+    @Test
+    public void sent() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Petr Arsentev", "eDer3432f"),
+                new Account("142", ",Petr Arsentev", "000001")
+        );
+        HashSet<Account> expect = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Petr Arsentev", "eDer3432f"),
+                        new Account("142", "Petr Arsentev", "000001")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expect));
+    }
+
+    @Test
+    public void deleteDuplicate() {
+        List<Account> accounts = Arrays.asList(
+                new Account("123", "Anastasia Levina", "q0w9er555"),
+                new Account("123", "Anastasia Levina", "q0w9er555"),
+                new Account("125", "Anastasia Levina", "q0w9er")
+        );
+        HashSet<Account> expected = new HashSet<>(
+                Arrays.asList(
+                        new Account("123", "Anastasia Levina", "q0w9er555"),
+                        new Account("125", "Anastasia Levina", "q0w9er")
+                )
+        );
+        assertThat(NotifyAccount.sent(accounts), is(expected));
+    }
+}

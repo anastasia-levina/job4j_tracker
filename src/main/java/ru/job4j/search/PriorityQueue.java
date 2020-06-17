695497694ab738336350ba4301d5831d62193762
anastasia-levina <a.belov.nsk@yandex.ru>
2020-06-17 17:54:50 +0300

    2. Рефакторинг - Очередь с приоритетом на LinkedList[41670#252263]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -17,7 +17,7 @@ public class PriorityQueue {
         for (Task element : tasks) {
             if (element.getPriority() < task.getPriority()) {
                 index++;
-            }
+            } else break;
         }
         this.tasks.add(index, task);
     }

811a0d3ce389d664a896f28f3f871526782004ce
anastasia-levina <a.belov.nsk@yandex.ru>
2020-06-17 21:25:26 +0300

    2. Комбинированный компаратор.[10036#252259]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,109 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+
+    @Test
+    public void whenComparatorDescByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void compareAscByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new JobAscByName());
+        List<Job> expect = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        assertThat(jobs, is(expect));
+    }
+
+    @Test
+    public void compareAscByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new JobAscByPriority());
+        List<Job> expect = Arrays.asList(
+                new Job("X task", 0),
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 2),
+                new Job("Fix bug", 4)
+
+        );
+        assertThat(jobs, is(expect));
+    }
+
+    @Test
+    public void compareDescByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new JobDescByName());
+        List<Job> expect = Arrays.asList(
+                new Job("X task", 0),
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2)
+
+        );
+        assertThat(jobs, is(expect));
+    }
+
+    @Test
+    public void compareDescByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
+        );
+        jobs.sort(new JobDescByPriority());
+        List<Job> expect = Arrays.asList(
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("Fix bug", 1),
+                new Job("X task", 0)
+        );
+        assertThat(jobs, is(expect));
+    }
+}

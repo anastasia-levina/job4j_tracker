d97fc08a61e2b2da6f28f613eb0e47523003b10e
anastasia-levina <a.belov.nsk@yandex.ru>
2020-07-10 15:42:23 +0300

    4. Зона видимости в лямбда и исключения[70609#252123]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+/*public class Departments {
+    HashSet<String> tmp = new HashSet<>();
+    for (String value : deps) {
+        String start = "";
+        for (String el : value.split("/")) {
+            // tmp.add start + "/" + el
+        }
+    }
+    return new ArrayList<>(tmp);
+}
+
+    public static void sortAsc(List<String> orgs) {
+    }
+
+    public static void sortDesc(List<String> orgs) {
+    }
+}
+*/

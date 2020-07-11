d8a6ee76168f091cddda56a46d8482d35340444e
anastasia-levina <a.belov.nsk@yandex.ru>
2020-07-11 18:42:02 +0300

    2.0. Рефакторинг - Встроенные функциональные интерфейсы.[249209#252126]

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -6,23 +6,25 @@ import java.util.List;
 
 public class SearchAtt {
 
-    public static List<Attachment> filterSize(List<Attachment> list, Predicate<Attachment> predicate) {
+    public static List<Attachment> filterSize(List<Attachment> list) {
         List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getSize() > 100) {
-                predicate.test(att);
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return (attachment.getSize() > 100);
             }
-        }
+        };
         return filter(rsl, predicate);
     }
 
-    public static List<Attachment> filterName(List<Attachment> list, Predicate<Attachment> predicate) {
+    public static List<Attachment> filterName(List<Attachment> list) {
         List<Attachment> rsl = new ArrayList<>();
-        for (Attachment att : list) {
-            if (att.getName().contains("bug")) {
-                predicate.test(att);
+        Predicate<Attachment> predicate = new Predicate<Attachment>() {
+            @Override
+            public boolean test(Attachment attachment) {
+                return (attachment.getName().contains("bug"));
             }
-        }
+        };
         return filter(rsl, predicate);
     }
 

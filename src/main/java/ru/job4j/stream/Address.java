46ffec6cc4875a6f0116d6f3921c5e3c69c9acb4
anastasia-levina <a.belov.nsk@yandex.ru>
2020-07-18 19:57:45 +0300

    3. Уникальность элементов и сортировка.[110225#252135]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -1,10 +1,13 @@
 package ru.job4j.stream;
 
+import java.util.Objects;
 
 public class Address {
     private String city;
     private String street;
+
     private int home;
+
     private int apartment;
 
     public Address(String city, String street, int home, int apartment) {
@@ -12,5 +15,53 @@ public class Address {
         this.street = street;
         this.home = home;
         this.apartment = apartment;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getHome() {
+        return home;
+    }
+
+    public void setHome(int home) {
+        this.home = home;
+    }
+
+    public int getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(int apartment) {
+        this.apartment = apartment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return city.equals(address.city) &&
+                street.equals(address.street) &&
+                home == address.home &&
+                apartment == address.apartment;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
     }
 }
--- src/test/java/ru/job4j/stream/ProfileTest.java
+++ src/test/java/ru/job4j/stream/ProfileTest.java
@@ -18,7 +18,21 @@ public class ProfileTest {
         Profile client2 = new Profile(addressClient2);
         List<Profile> profileClients = Arrays.asList(client1, client2);
         List<Address> rsl = Profiles.collect(profileClients);
-        List<Address> expected = Arrays.asList(addressClient1, addressClient2);
+        List<Address> expected = Arrays.asList(addressClient2, addressClient1);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void gettingAddressSort() {
+        Address addressClient1 = new Address("Novosibirsk", "Lenina", 55, 84);
+        Profile client1 = new Profile(addressClient1);
+        Address addressClient2 = new Address("Moscow", "Tverskaya", 31, 5);
+        Profile client2 = new Profile(addressClient2);
+        Address addressClient3 = new Address("Moscow", "Tverskaya", 31, 5);
+        Profile client3 = new Profile(addressClient3);
+        List<Profile> profileClients = Arrays.asList(client1, client2, client3);
+        List<Address> rsl = Profiles.collect(profileClients);
+        List<Address> expected = Arrays.asList(addressClient2, addressClient1);
         assertThat(rsl, is(expected));
     }
 }

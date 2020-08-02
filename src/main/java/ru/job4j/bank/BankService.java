df9f0d9a681c490f4e116be919ff99f2e78e64fa
anastasia-levina <a.belov.nsk@yandex.ru>
2020-08-03 01:16:26 +0300

    3. Рефакторинг - Optional в банковских переводах.[214711#252143]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -26,25 +26,26 @@ public class BankService {
                 .findFirst();
     }
 
-    public Account findByRequisite(String passport, String requisite) {
+    public Optional<Account> findByRequisite(String passport, String requisite) {
         return findByPassport(passport)
                 .map(usr -> users.get(usr))
                 .orElse(Collections.emptyList())
                 .stream()
                 .filter(account -> requisite.equals(account.getRequisite()))
-                .findFirst()
-                .orElse(null);
+                .findFirst();
     }
 
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
-        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
-        Account destAccount = findByRequisite(destPassport, destRequisite);
+        Optional<Account> srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Optional<Account> destAccount = findByRequisite(destPassport, destRequisite);
+        double srcBalance = srcAccount.map(Account::getBalance).orElse(0D);
+        double destBalance = destAccount.map(Account::getBalance).orElse(0D);
 
-        if (srcAccount != null && destAccount != null && amount <= srcAccount.getBalance()) {
-            srcAccount.setBalance(srcAccount.getBalance() - amount);
-            destAccount.setBalance(destAccount.getBalance() + amount);
+        if (amount <= srcBalance) {
+            srcAccount.ifPresent(account -> account.setBalance(srcBalance - amount));
+            destAccount.ifPresent(account -> account.setBalance(destBalance + amount));
             rsl = true;
         }
         return rsl;
--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -22,7 +22,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertNull(bank.findByRequisite("34", "5546"));
+        bank.findByRequisite("34", "5546").ifPresent(account -> fail("Optional should be empty"));
     }
 
     @Test
@@ -31,7 +31,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        assertThat(bank.findByRequisite("3434", "5546").getBalance(), is(150D));
+        assertThat(bank.findByRequisite("3434", "5546").map(Account::getBalance), is(Optional.of(150D)));
     }
 
     @Test
@@ -42,6 +42,6 @@ public class BankServiceTest {
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
         bank.addAccount(user.getPassport(), new Account("113", 50D));
         bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+        assertThat(bank.findByRequisite(user.getPassport(), "113").map(Account::getBalance), is(Optional.of(200D)));
     }
 }

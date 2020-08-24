038618c337fed98445546acce5ec3899af1f206f
anastasia-levina <a.belov.nsk@yandex.ru>
2020-08-24 15:40:12 +0300

    Отсортировать департаменты[34352#252253]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        HashSet<String> tmp = new HashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if (start.equals("")) {
+                    start = el;
+                } else {
+                    start += "/" + el;
+                }
+                tmp.add(start);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
